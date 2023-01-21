d4b9253effb148b1b1dc76df1a9922c7eab8a6b1
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-20 21:14:10 -0600

    added weather api, not responding

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,57 @@
-import './app.scss'
-
 let weather = {
-    apiKey: "1aa9e2f867c7bb69b42d05b93ecab9c5",
+    apiKey: "e5b4acfc6d86de0eb7656fdc7bed7c0d",
     fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
             + city 
             + "&units=imperial&appid="
-            + this.apiKey     
+            + this.apiKey  
         )
             .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
+            .then((data) => this.displayWeather(data))
     },
     displayWeather: (data) => {
         const { name } = data;
-        const { icon, description } = data.weather[0]
+        const { icon, description } = data.weather
         const { temp } = data.main
         console.log(name, icon, description, temp)
-        document.querySelector(".city").innerHTML = "weather in " + name;
+        document.querySelector(".city").innerHTML = "weather in " + city
         document.querySelector(".icon").src =
             "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerHTML = description;
-        document.querySelector(".temp").innerHTML = temp + "°";
+        document.querySelector(".temp").innerHTML = temp + "°F";
+        document.querySelector('.description')
        
         
     },
     search: function() {
-        this.fetchWeather(document.querySelector(".search").value);
+        this.fetchWeather(document.querySelector(".searchbar").value);
     }
 };
 
-document.querySelector(".search").addEventListener("click", function () {
+document.querySelector('.temp-today').addEventListener("click", function () {
     weather.search();
+});
+
+document.querySelector('.temp-today').addEventListener('keyup', function (event) {
+    if (event.key = 'enter') {
+        weather.search()
+    }
+})
+
+weather.fetchWeather('Houston')
+
+let hotCoffee  {
+
+}
+
+document.querySelector('.hot').addEventListener("click", function () {
+    hotCoffee.random();
+});
+
+let icedCoffee {
+    
+}
+
+document.querySelector('.iced').addEventListener("click", function () {
+    icedCoffee.random();
 });
